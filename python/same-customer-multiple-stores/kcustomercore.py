6fde16e6ffc31a1276e38390b600799becb88ae5
Animesh Pathak <animesh.pathak@mykaarma.com>
2020-10-21 02:16:29 -0700

    rename file. sample CSV

--- python/same-customer-multiple-stores/kcustomercore.py
+++ python/same-customer-multiple-stores/kcustomercore.py
@@ -27,16 +27,15 @@ def add_customer(creds, dept_uid, customer, DEBUG = False):
 
     This method will take the credentials (username, password, and base URL)
     the departmentUUID, and a DICT matching the customer object structure, and PUT it 
-    in the API. An optional DEBUG boolean can be passed that will result in no network traffic, 
-    but dumping of the HTTP request on stdout.
+    in the API. An optional DEBUG boolean can be passed that will result in dumping of 
+    the HTTP response on stdout.
     """
 
     url = "%s/department/%s/customer" % (creds[KEY_BASE_URL],dept_uid)
     r = requests.put(url,auth=(creds[KEY_USERNAME],creds[KEY_PASSWORD]), json = customer)
     if DEBUG:
         print(r.json())
-    return r.json()['response']
-
+    return r.json()['customerUuid']
 
 if __name__ == "__main__":
     print('Sorry, no direct usage available. Import and then call the methods.')
